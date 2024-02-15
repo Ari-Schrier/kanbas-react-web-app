c50c49db5b7f70f27687c6f79a8550907cdf302e
Aidan Schrier <schrier.a@northeastern.edu>
2024-02-14 23:39:49 -0500

    Added SO MANY FILES

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -2,6 +2,26 @@ import VariablesAndConstants
   from "./variables/VariablesAndConstants";
 import VariableTypes from "./variables/VariableTypes";
 import BooleanVariables from "./variables/BooleanVariables";
+import IfElse from "./conditionals/IfElse";
+import TernaryOperator from "./conditionals/TernaryOperator";
+import ES5Function from "./functions/ES5Functions";
+import ArrowFunctions from "./functions/ArrowFunctions";
+import ImpliedReturns from "./functions/ImpliedReturns";
+import FunctionParenthesisAndParameters from "./functions/FunctionParenthesisAndParameters";
+import WorkingWithArrays from "./arrays/WorkingWithArrays";
+import ArrayIndexAndLength from "./arrays/ArrayIndexAndLength";
+import AddingAndRemovingDataToFromArrays from "./arrays/AddingAndRemovingDataToFromArrays";
+import ForLoop from "./arrays/ForLoop";
+import MapFunction from "./arrays/MapFunction";
+import JsonStringify from "./json/JsonStringify";
+import FindFunction from "./arrays/FindFunction";
+import FindIndex from "./arrays/FindIndex";
+import FilterFunction from "./arrays/FilterFunction";
+import TemplateLiterals from "../string/TemplateLiterals";
+import House from "./json/House";
+import Spreading from "./json/Spreading";
+import Destructing from "./json/Destructing";
+import FunctionDestructing from "./functions/FunctionDestructing";
 
 function JavaScript() {
     console.log('Hello World!');
@@ -11,6 +31,26 @@ function JavaScript() {
           <VariablesAndConstants />
           <VariableTypes/>
           <BooleanVariables/>
+          <IfElse />
+          <TernaryOperator />
+          <ES5Function />
+          <ArrowFunctions />
+          <ImpliedReturns />
+          <FunctionParenthesisAndParameters />
+          <WorkingWithArrays />
+          <ArrayIndexAndLength />
+          <AddingAndRemovingDataToFromArrays />
+          <ForLoop />
+          <MapFunction />
+          <JsonStringify />
+          <FindFunction />
+          <FindIndex />
+          <FilterFunction />
+          <TemplateLiterals />
+          <House />
+          <Spreading />
+          <Destructing />
+          <FunctionDestructing />
        </div>
     );
  }
